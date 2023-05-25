bc1696753f74370a76b909d16ea6228b9ef12c69
Wouch <65008462+BlueSkyAndSomeCurses@users.noreply.github.com>
2023-05-25 22:51:02 +0300

    dmenu now takes colors from pywal and auto linking of dotfiles

--- gitinstalls.sh
+++ gitinstalls.sh
@@ -9,3 +9,12 @@ cd ~/yay
 makepkg -si
 yay -Y --gendb
 yay -Syu --devel
+
+mv $HOME/.dotfiles/.zshrc $HOME/
+mv $HOME/.dotfiles/.xinitrc $HOME/
+mv $HOME/.dotfiles/.xprofile $HOME/
+mv $HOME/.dotfiles/.config/i3/config $HOME/.dotfiles/.config/i3/
+ln -s $HOME/.zshrc $HOME/.dotfiles/.zshrc
+ln -s $HOME/.xinitrc $HOME/.dotfiles/.xinitrc
+ln -s $HOME/.xprofile $HOME/.dotfiles/.xprofile
+ln -s $HOME/.config/i3/config $HOME/.dotfiles/.config/i3/config
