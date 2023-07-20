16851ea2a3c93d81f66a4cbaa0505553eaaed5a8
Wouch <noreply@noreply.xyz>
2023-07-20 23:49:04 +0300

    added auto install commands to compile st dwm dmenu

--- gitinstalls.sh
+++ gitinstalls.sh
@@ -14,6 +14,24 @@ git clone --depth=1 https://github.com/AstroNvim/AstroNvim $HOME/.config/nvim
 cp -r user/ $HOME/.config/nvim/lua/
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.config/zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.config/zsh/zsh-syntax-highlighting
+
+git clone https://github.com/BlueSkyAndSomeCurses/dwm.git $HOME/.local/src/dwm
+git clone https://github.com/BlueSkyAndSomeCurses/dwmblocks.git $HOME/.local/src/dwmblocks
+git clone https://github.com/BlueSkyAndSomeCurses/st.git $HOME/.local/src/st
+git clone https://github.com/BlueSkyAndSomeCurses/dmenu.git $HOME/.local/src/dmenu
+
+cd $HOME/.local/src/dwm
+sudo make clean install
+
+cd $HOME/.local/src/dwmblocks
+sudo make clean install
+
+cd $HOME/.local/src/st
+sudo make clean install
+
+cd $HOME/.local/src/dmenu
+sudo make clean install
+
 git clone https://aur.archlinux.org/yay.git $HOME/.local/yay
 cd $HOME/.local/yay
 makepkg -si
